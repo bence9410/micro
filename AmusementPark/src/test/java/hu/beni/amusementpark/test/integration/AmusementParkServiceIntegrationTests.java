5536e0e9b48fe4c179c7ae7a22a9f13e3ff48e24
Németh Bence <nembence1994@gmail.com>
2018-02-08 16:00:31 +0100

    Remove unused import.

--- AmusementPark/src/test/java/hu/beni/amusementpark/test/integration/AmusementParkServiceIntegrationTests.java
+++ AmusementPark/src/test/java/hu/beni/amusementpark/test/integration/AmusementParkServiceIntegrationTests.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
