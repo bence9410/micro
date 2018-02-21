8d934be7af5a9f58837ba53849a40d6e2d0f2101
Németh Bence <nembence1994@gmail.com>
2018-02-21 15:32:37 +0100

    Modify ZooController

--- Zoo/src/main/java/hu/beni/zoo/controller/ZooController.java
+++ Zoo/src/main/java/hu/beni/zoo/controller/ZooController.java
@@ -1,17 +1,16 @@
 package hu.beni.zoo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/zoo")
 public class ZooController {
-
-	@GetMapping
-	public String get(@RequestHeader(name = "Authorization") String auth) {
-		return "Hello " + auth;
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello from Zoo!";
 	}
 
 }
