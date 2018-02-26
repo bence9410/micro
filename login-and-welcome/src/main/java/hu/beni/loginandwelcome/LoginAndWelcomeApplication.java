7b2de8a3352b745eb37fb8fd7294c135966149ce
Németh Bence <nembence1994@gmail.com>
2018-02-26 15:21:32 +0100

    Move welcome page to new project from gateway.

---
+++ login-and-welcome/src/main/java/hu/beni/loginandwelcome/LoginAndWelcomeApplication.java
@@ -0,0 +1,12 @@
+package hu.beni.loginandwelcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndWelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAndWelcomeApplication.class, args);
+	}
+}
