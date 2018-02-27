64c793cf7a57f15a2aff9db3f540b08f70fd6c8b
Németh Bence <nembence1994@gmail.com>
2018-02-27 11:52:32 +0100

    Add method security, only say hello to ADMINs.

--- amusement-park-micro/src/main/java/hu/beni/amusementparkmicro/config/ResourceServerConfig.java
+++ amusement-park-micro/src/main/java/hu/beni/amusementparkmicro/config/ResourceServerConfig.java
@@ -2,11 +2,13 @@ package hu.beni.amusementparkmicro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig {
 
 	@Bean
--- amusement-park-micro/src/main/java/hu/beni/amusementparkmicro/controller/AmusementParkController.java
+++ amusement-park-micro/src/main/java/hu/beni/amusementparkmicro/controller/AmusementParkController.java
@@ -1,5 +1,6 @@
 package hu.beni.amusementparkmicro.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AmusementParkController {
 
 	@GetMapping("/hello")
+	@PreAuthorize("hasRole('ADMIN')")
 	public String hello() {
 		return "Hello from AmusementPark!";
 	}
