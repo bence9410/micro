b12a815a90fef28314af48246bec4d929b187e46
Németh Bence <nembence1994@gmail.com>
2018-03-13 14:14:23 +0100

    Add docker profile, use 80 port in docker.

--- amusement-park-micro/src/main/java/hu/beni/amusementparkmicro/config/ResourceServerConfig.java
+++ amusement-park-micro/src/main/java/hu/beni/amusementparkmicro/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package hu.beni.amusementparkmicro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
@@ -12,9 +13,15 @@ import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 public class ResourceServerConfig {
 
 	@Bean
-	public RemoteTokenServices remoteTokenServices() {
+	public RemoteTokenServices remoteTokenServices(Environment environment) {
+		String tokenHost;
+		if (environment.getActiveProfiles().length == 0) {
+			tokenHost = "localhost";
+		} else {
+			tokenHost = "oauth2";
+		}
 		RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
-		remoteTokenServices.setCheckTokenEndpointUrl("http://oauth2:9999/uaa/oauth/check_token");
+		remoteTokenServices.setCheckTokenEndpointUrl("http://" + tokenHost + ":9999/uaa/oauth/check_token");
 		remoteTokenServices.setClientId("beni");
 		remoteTokenServices.setClientSecret("benisecret");
 		return remoteTokenServices;
