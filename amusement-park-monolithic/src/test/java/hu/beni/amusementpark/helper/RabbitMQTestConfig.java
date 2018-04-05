ec94f24e26444ba072f439d37d70a1de420abbf7
Németh Bence <nembence1994@gmail.com>
2018-04-05 10:37:09 +0200

    Remove unused imports.

--- amusement-park-monolithic/src/test/java/hu/beni/amusementpark/helper/RabbitMQTestConfig.java
+++ amusement-park-monolithic/src/test/java/hu/beni/amusementpark/helper/RabbitMQTestConfig.java
@@ -19,8 +19,6 @@ import org.springframework.context.annotation.Profile;
 
 import hu.beni.amusementpark.archive.ArchiveSender;
 import hu.beni.amusementpark.archive.impl.RabbitMQArchiveSender;
-import hu.beni.amusementpark.entity.AmusementPark;
-import hu.beni.dto.AmusementParkDTO;
 import hu.beni.dto.ArchiveAmusementParkDTO;
 import lombok.Getter;
 
