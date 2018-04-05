b8d91653033ff735f22282d12bab533d8876a9b6
Németh Bence <nembence1994@gmail.com>
2018-04-05 15:16:47 +0200

    Rename test class and package in amusement-park.

--- amusement-park/src/test/java/hu/beni/amusementpark/AmusementParkApplicationTests.java
+++ amusement-park/src/test/java/hu/beni/amusementpark/AmusementParkApplicationTests.java
@@ -1,4 +1,4 @@
-package hu.beni.amusementparkmicro;
+package hu.beni.amusementpark;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AmusementParkMicroApplicationTests {
+public class AmusementParkApplicationTests {
 
 	@Test
 	public void contextLoads() {
