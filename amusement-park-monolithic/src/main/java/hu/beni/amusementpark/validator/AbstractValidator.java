5f682757b84c4c857c22a08391c30fe837299ad2
Németh Bence <nembence1994@gmail.com>
2018-05-18 12:11:45 +0200

    Modify AbstractValidator's constructor access to protected.

--- amusement-park-monolithic/src/main/java/hu/beni/amusementpark/validator/AbstractValidator.java
+++ amusement-park-monolithic/src/main/java/hu/beni/amusementpark/validator/AbstractValidator.java
@@ -2,9 +2,10 @@ package hu.beni.amusementpark.validator;
 
 import org.springframework.validation.Validator;
 
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AbstractValidator<T> implements Validator {
 
 	private final Class<T> targetClass;
