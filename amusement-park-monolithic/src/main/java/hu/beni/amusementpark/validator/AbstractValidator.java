90f25fe46cc7517dbc7ab88784f38dffbbf4aab4
Németh Bence <nembence1994@gmail.com>
2018-05-18 12:14:07 +0200

    Add restriction to AbstractValidator's type parameter.

--- amusement-park-monolithic/src/main/java/hu/beni/amusementpark/validator/AbstractValidator.java
+++ amusement-park-monolithic/src/main/java/hu/beni/amusementpark/validator/AbstractValidator.java
@@ -1,12 +1,13 @@
 package hu.beni.amusementpark.validator;
 
+import org.springframework.hateoas.ResourceSupport;
 import org.springframework.validation.Validator;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-public abstract class AbstractValidator<T> implements Validator {
+public abstract class AbstractValidator<T extends ResourceSupport> implements Validator {
 
 	private final Class<T> targetClass;
 
