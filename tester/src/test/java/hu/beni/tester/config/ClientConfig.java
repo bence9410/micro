348d3b4686d5a6695d939ee06e84095dcc24e0fc
Németh Bence <nembence1994@gmail.com>
2018-06-04 16:26:55 +0200

    Remove left over Autowired annotation.

--- tester/src/test/java/hu/beni/tester/config/ClientConfig.java
+++ tester/src/test/java/hu/beni/tester/config/ClientConfig.java
@@ -4,7 +4,6 @@ import static org.springframework.beans.factory.config.ConfigurableBeanFactory.S
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -24,7 +23,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ClientConfig {
 
-	@Autowired
 	private final ObjectMapper mapper;
 
 	@PostConstruct
