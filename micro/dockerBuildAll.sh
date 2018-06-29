5420efc57e31204c240be6b36d4ff56c73bab512
Németh Bence <nembence1994@gmail.com>
2018-06-29 11:21:59 +0200

    Add docker database run to runAll.sh, skip tests in dockerBuildAll.sh, Add contextLoads test to Gateway.

--- micro/dockerBuildAll.sh
+++ micro/dockerBuildAll.sh
@@ -1,9 +1,9 @@
 gnome-terminal --maximize \
---tab -e "bash -c 'cd gateway/; mvn clean package dockerfile:build; exec bash'" \
---tab -e "bash -c 'cd oauth2/; mvn clean package dockerfile:build; exec bash'" \
---tab -e "bash -c 'cd welcome/; mvn clean package dockerfile:build; exec bash'" \
---tab -e "bash -c 'cd amusement-park-ui/; mvn clean package dockerfile:build; exec bash'" \
---tab -e "bash -c 'cd zoo-ui/; mvn clean package dockerfile:build; exec bash'" \
---tab -e "bash -c 'cd amusement-park/; mvn clean package dockerfile:build; exec bash'" \
---tab -e "bash -c 'cd zoo/; mvn clean package dockerfile:build; exec bash'" \
+--tab -e "bash -c 'cd gateway/; mvn clean package -DskipTests dockerfile:build; exec bash'" \
+--tab -e "bash -c 'cd oauth2/; mvn clean package -DskipTests dockerfile:build; exec bash'" \
+--tab -e "bash -c 'cd welcome/; mvn clean package -DskipTests dockerfile:build; exec bash'" \
+--tab -e "bash -c 'cd amusement-park-ui/; mvn clean package -DskipTests dockerfile:build; exec bash'" \
+--tab -e "bash -c 'cd zoo-ui/; mvn clean package -DskipTests dockerfile:build; exec bash'" \
+--tab -e "bash -c 'cd amusement-park/; mvn clean package -DskipTests dockerfile:build; exec bash'" \
+--tab -e "bash -c 'cd zoo/; mvn clean package -DskipTests dockerfile:build; exec bash'" \
 --tab -e "bash -c 'cd database/; docker build -t benike/database-oracle-xe-11g .; exec bash'"
--- micro/runAll.sh
+++ micro/runAll.sh
@@ -5,4 +5,5 @@ gnome-terminal --maximize \
 --tab -e "bash -c 'cd amusement-park-ui/; mvn clean spring-boot:run; exec bash'" \
 --tab -e "bash -c 'cd zoo-ui/; mvn clean spring-boot:run; exec bash'" \
 --tab -e "bash -c 'cd amusement-park/; mvn clean spring-boot:run; exec bash'" \
---tab -e "bash -c 'cd zoo/; mvn clean spring-boot:run; exec bash'"
+--tab -e "bash -c 'cd zoo/; mvn clean spring-boot:run; exec bash'"  \
+--tab -e "bash -c 'docker run -d -p 1521:1521 benike/database-oracle-xe-11g'"
