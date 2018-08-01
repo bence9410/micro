1bc2080119246b0c8ff2fb7f94253aeebea0d080
Németh Bence <nembence1994@gmail.com>
2018-08-01 15:40:21 +0200

    Add some more jpa criteria example and move the existing ones to a new class.

---
+++ amusement-park-monolithic/src/test/java/hu/beni/amusementpark/test/JPASpecificationTests.java
@@ -0,0 +1,207 @@
+package hu.beni.amusementpark.test;
+
+import static hu.beni.amusementpark.helper.ValidEntityFactory.createAmusementParkWithAddress;
+import static hu.beni.amusementpark.helper.ValidEntityFactory.createMachine;
+import static hu.beni.amusementpark.helper.ValidEntityFactory.createVisitor;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import hu.beni.amusementpark.constants.StringParamConstants;
+import hu.beni.amusementpark.entity.AmusementPark;
+import hu.beni.amusementpark.entity.GuestBookRegistry;
+import hu.beni.amusementpark.entity.Machine;
+import hu.beni.amusementpark.entity.Visitor;
+import hu.beni.amusementpark.repository.AmusementParkRepository;
+import hu.beni.amusementpark.repository.GuestBookRegistryRepository;
+import hu.beni.amusementpark.repository.MachineRepository;
+import hu.beni.amusementpark.repository.VisitorRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+public class JPASpecificationTests {
+
+	private static final AmusementPark BASE_AMUSEMENT_PARK = createAmusementParkWithAddress();
+	private static List<AmusementPark> amusementParksWithMachineForChildren;
+
+	@Autowired
+	private AmusementParkRepository amusementParkRepository;
+
+	@Autowired
+	private MachineRepository machineRepository;
+
+	@Autowired
+	private VisitorRepository visitorRepository;
+
+	@Autowired
+	private GuestBookRegistryRepository guestBookRegistryRepository;
+
+	@PostConstruct
+	public void init() {
+		if (amusementParksWithMachineForChildren == null) {
+			createSampleData();
+		}
+	}
+
+	private void createSampleData() {
+		List<AmusementPark> amusementParks = IntStream.range(1, 11).mapToObj(i -> {
+			AmusementPark amusementPark = createAmusementParkWithAddress();
+			amusementPark.setName(amusementPark.getName() + i * i); // name1, ... name16, ... name49, ... name100
+			amusementPark.setCapital(amusementPark.getCapital() * i); // 3000, 6000, ... 30000
+			return amusementPark;
+		}).map(amusementParkRepository::save).collect(Collectors.toList());
+
+		Visitor v1 = visitorRepository.save(createVisitor());
+		Visitor v2 = visitorRepository.save(createVisitor());
+
+		amusementParksWithMachineForChildren = new ArrayList<>();
+		for (int i = 0; i < amusementParks.size(); i++) {
+			AmusementPark amusementPark = amusementParks.get(i);
+			addMachine(amusementPark, false);
+			addGuestBookRegistry(amusementPark, v1);
+
+			if (i == 2 || i == 7) {
+				addMachine(amusementPark, true);
+				addMachine(amusementPark, true);
+				addGuestBookRegistry(amusementPark, v2);
+				amusementParksWithMachineForChildren.add(amusementPark);
+			}
+		}
+	}
+
+	private void addMachine(AmusementPark amusementPark, boolean machineForChildren) {
+		Machine machine = createMachine();
+		machine.setAmusementPark(amusementPark);
+		if (machineForChildren) {
+			machine.setMinimumRequiredAge(10);
+		}
+		machineRepository.save(machine);
+	}
+
+	private void addGuestBookRegistry(AmusementPark amusementPark, Visitor visitor) {
+		guestBookRegistryRepository
+				.save(GuestBookRegistry.builder().textOfRegistry(StringParamConstants.OPINION_ON_THE_PARK)
+						.amusementPark(amusementPark).visitor(visitor).build());
+	}
+
+	@Test(expected = IncorrectResultSizeDataAccessException.class)
+	public void findOneNotRetunsUniqueResult() {
+		amusementParkRepository.findOne(like("name", BASE_AMUSEMENT_PARK.getName() + "1%"));
+	}
+
+	@Test
+	public void findOneCapitalGreaterThan() {
+		int capital = 28000;
+		AmusementPark result = amusementParkRepository.findOne(greaterThan("capital", capital)).get();
+		assertTrue(result.getCapital() > capital);
+	}
+
+	@Test
+	public void findOneNameLikeAndCapitalGreaterThan() {
+		int capital = 14000;
+
+		Specification<AmusementPark> spec = like("name", BASE_AMUSEMENT_PARK.getName() + "1%");
+		spec = spec.and(greaterThan("capital", capital));
+
+		AmusementPark amusementPark = amusementParkRepository.findOne(spec).get();
+		assertTrue(amusementPark.getName().startsWith(BASE_AMUSEMENT_PARK.getName() + "1"));
+		assertTrue(amusementPark.getCapital() > capital);
+	}
+
+	@Test
+	public void findOneNameLikeAndCapitalGreaterThanAndAddressCityLike() {
+		int capital = 14000;
+
+		Specification<AmusementPark> spec = (Root<AmusementPark> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+			root.fetch("address");
+			return null;
+		};
+
+		spec = spec.and(like("name", BASE_AMUSEMENT_PARK.getName() + "1%")).and(greaterThan("capital", capital))
+				.and(like("address.city", "Buda%"));
+
+		AmusementPark amusementPark = amusementParkRepository.findOne(spec).get();
+		assertTrue(amusementPark.getName().startsWith(BASE_AMUSEMENT_PARK.getName() + "1"));
+		assertTrue(amusementPark.getCapital() > capital);
+		assertTrue(amusementPark.getAddress().getCity().startsWith("Buda"));
+	}
+
+	@Test
+	public void findAllWithMachineForChildren() {
+
+		Specification<AmusementPark> spec = (Root<AmusementPark> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+			Subquery<Machine> subQuery = query.subquery(Machine.class);
+			Root<Machine> subRoot = subQuery.from(Machine.class);
+			subQuery.select(subRoot);
+			subQuery.where(cb.equal(root.get("id"), subRoot.get("amusementPark").get("id")),
+					cb.lt(subRoot.get("minimumRequiredAge"), 18));
+			query.orderBy(cb.asc(root.get("id")));
+			return cb.exists(subQuery);
+		};
+
+		List<AmusementPark> resultWithSubQuery = amusementParkRepository.findAll(spec);
+
+		spec = (Root<AmusementPark> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+			Join<AmusementPark, Machine> joinMachines = root.join("machines");
+			joinMachines.on(cb.lt(joinMachines.get("minimumRequiredAge"), 18));
+			query.distinct(true);
+			query.orderBy(cb.asc(root.get("id")));
+			return null;
+		};
+
+		List<AmusementPark> resultWithJoin = amusementParkRepository.findAll(spec);
+
+		assertEquals(amusementParksWithMachineForChildren.size(), resultWithJoin.size());
+		assertEquals(amusementParksWithMachineForChildren.size(), resultWithSubQuery.size());
+
+		assertEquals(amusementParksWithMachineForChildren, resultWithSubQuery);
+		assertEquals(amusementParksWithMachineForChildren, resultWithJoin);
+
+	}
+
+	private <T> Specification<T> like(String fieldName, String param) {
+		return (Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> cb.like(createPath(root, fieldName),
+				param);
+	}
+
+	private <T> Specification<T> greaterThan(String fieldName, int param) {
+		return (Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> cb.greaterThan(createPath(root, fieldName),
+				param);
+	}
+
+	private <T, Y> Path<Y> createPath(Root<T> root, String fieldName) {
+		Path<Y> path = null;
+		if (fieldName.indexOf('.') == -1) {
+			path = root.get(fieldName);
+		} else {
+			String[] parts = fieldName.split("\\.");
+			path = root.get(parts[0]);
+			for (int i = 1; i < parts.length; i++) {
+				path = path.get(parts[i]);
+			}
+		}
+		return path;
+	}
+
+}
--- amusement-park-monolithic/src/test/java/hu/beni/amusementpark/test/integration/service/AmusementParkServiceIntegrationTests.java
+++ amusement-park-monolithic/src/test/java/hu/beni/amusementpark/test/integration/service/AmusementParkServiceIntegrationTests.java
@@ -1,22 +1,13 @@
 package hu.beni.amusementpark.test.integration.service;
 
 import static hu.beni.amusementpark.helper.ValidEntityFactory.createAmusementParkWithAddress;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Path;
-import javax.persistence.criteria.Root;
-
-import org.hibernate.LazyInitializationException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,19 +16,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.transaction.support.TransactionTemplate;
 
-import hu.beni.amusementpark.entity.Address;
 import hu.beni.amusementpark.entity.AmusementPark;
 import hu.beni.amusementpark.repository.AmusementParkRepository;
 import hu.beni.amusementpark.service.AmusementParkService;
 import hu.beni.amusementpark.test.integration.AbstractStatementCounterTests;
 
 public class AmusementParkServiceIntegrationTests extends AbstractStatementCounterTests {
-
-	@Autowired
-	private TransactionTemplate transactionTemplate;
 
 	@Autowired
 	private AmusementParkService amusementParkService;
@@ -117,71 +102,6 @@ public class AmusementParkServiceIntegrationTests extends AbstractStatementCount
 		return page;
 	}
 
-	@Test
-	public void specificationTest() {
-		String name = "asd";
-		int capital = 2000;
-
-		createAmusementParkWithAddressSetParam(name + "123", name + "45");
-
-		createAmusementParkWithAddressSetParam(name + "67", name + "89");
-
-		reset();
-
-		findOne(fieldLikeParam(AmusementPark.class, "name", name + "1%"));
-
-		select++;
-		assertStatements();
-		assertNotNull(amusementPark);
-		assertTrue(amusementPark.getName().startsWith(name + "1"));
-
-		Specification<AmusementPark> nameLikeAndAddressCityLikeAndCapitalGreaterThan = Specification
-				.where(fieldLikeParam(AmusementPark.class, "name", name + "%"))
-				.and(fieldLikeParam(AmusementPark.class, "address.city", name + "%"))
-				.and(fieldGreaterThanParam(AmusementPark.class, "capital", capital));
-
-		List<AmusementPark> lazyAmusementParks = amusementParkService
-				.findAll(nameLikeAndAddressCityLikeAndCapitalGreaterThan);
-		assertFalse(lazyAmusementParks.isEmpty());
-
-		select++;
-		assertStatements();
-
-		AmusementPark lazyAmusementPark = lazyAmusementParks.get(0);
-
-		Address lazyAddress = lazyAmusementPark.getAddress();
-
-		assertNotNull(lazyAddress.getId());
-		assertEquals(lazyAmusementPark.getId(), lazyAddress.getId());
-
-		assertThatThrownBy(() -> lazyAddress.getCity()).isInstanceOf(LazyInitializationException.class);
-
-		transactionTemplate.execute(status -> {
-			List<AmusementPark> amusementParks = amusementParkService
-					.findAll(nameLikeAndAddressCityLikeAndCapitalGreaterThan);
-			assertFalse(amusementParks.isEmpty());
-
-			for (AmusementPark a : amusementParks) {
-				assertTrue(a.getName().startsWith(name) && a.getAddress().getCity().startsWith(name)
-						&& a.getCapital() > capital);
-			}
-			return null;
-		});
-		select += 3;
-		assertStatements();
-	}
-
-	private void createAmusementParkWithAddressSetParam(String name, String addressCity) {
-		AmusementPark amusementPark = createAmusementParkWithAddress();
-		amusementPark.setName(name);
-		amusementPark.getAddress().setCity(name);
-		amusementParkService.save(amusementPark);
-	}
-
-	private void findOne(Specification<AmusementPark> specification) {
-		amusementPark = amusementParkService.findOne(specification);
-	}
-
 	private void createNineAmusementParkWithAscendantCapital() {
 		IntStream.range(1000, 1009).forEach(i -> {
 			AmusementPark amusementPark = createAmusementParkWithAddress();
@@ -194,27 +114,4 @@ public class AmusementParkServiceIntegrationTests extends AbstractStatementCount
 		return a -> assertTrue(min <= a.getCapital() && max >= a.getCapital());
 	}
 
-	private <T> Specification<T> fieldLikeParam(Class<T> clazz, String fieldName, String param) {
-		return (Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> cb.like(createPath(root, fieldName),
-				param);
-	}
-
-	private <T> Specification<T> fieldGreaterThanParam(Class<T> clazz, String fieldName, int param) {
-		return (Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> cb.greaterThan(createPath(root, fieldName),
-				param);
-	}
-
-	private <T, Y> Path<Y> createPath(Root<T> root, String fieldName) {
-		Path<Y> path = null;
-		if (fieldName.indexOf('.') == -1) {
-			path = root.get(fieldName);
-		} else {
-			String[] parts = fieldName.split("\\.");
-			path = root.get(parts[0]);
-			for (int i = 1; i < parts.length; i++) {
-				path = path.get(parts[i]);
-			}
-		}
-		return path;
-	}
 }
