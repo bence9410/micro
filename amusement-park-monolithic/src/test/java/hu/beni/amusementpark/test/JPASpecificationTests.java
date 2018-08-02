fb70cea0bb095d186955da0220358929b9130240
Németh Bence <nembence1994@gmail.com>
2018-08-02 12:58:02 +0200

    Generate and use Metamodels in Criteria queries.

--- amusement-park-monolithic/src/test/java/hu/beni/amusementpark/test/JPASpecificationTests.java
+++ amusement-park-monolithic/src/test/java/hu/beni/amusementpark/test/JPASpecificationTests.java
@@ -15,7 +15,6 @@ import javax.annotation.PostConstruct;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Root;
 import javax.persistence.criteria.Subquery;
 
@@ -29,9 +28,12 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import hu.beni.amusementpark.constants.StringParamConstants;
+import hu.beni.amusementpark.entity.Address_;
 import hu.beni.amusementpark.entity.AmusementPark;
+import hu.beni.amusementpark.entity.AmusementPark_;
 import hu.beni.amusementpark.entity.GuestBookRegistry;
 import hu.beni.amusementpark.entity.Machine;
+import hu.beni.amusementpark.entity.Machine_;
 import hu.beni.amusementpark.entity.Visitor;
 import hu.beni.amusementpark.repository.AmusementParkRepository;
 import hu.beni.amusementpark.repository.GuestBookRegistryRepository;
@@ -107,25 +109,29 @@ public class JPASpecificationTests {
 
 	@Test(expected = IncorrectResultSizeDataAccessException.class)
 	public void findOneNotRetunsUniqueResult() {
-		amusementParkRepository.findOne(like("name", BASE_AMUSEMENT_PARK.getName() + "1%"));
+		amusementParkRepository.findOne((Root<AmusementPark> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> cb
+				.like(root.get(AmusementPark_.name), BASE_AMUSEMENT_PARK.getName() + "1%"));
 	}
 
 	@Test
 	public void findOneCapitalGreaterThan() {
 		int capital = 28000;
-		AmusementPark result = amusementParkRepository.findOne(greaterThan("capital", capital)).get();
+		AmusementPark result = amusementParkRepository.findOne((Root<AmusementPark> root, CriteriaQuery<?> query,
+				CriteriaBuilder cb) -> cb.gt(root.get(AmusementPark_.capital), capital)).get();
 		assertTrue(result.getCapital() > capital);
 	}
 
 	@Test
 	public void findOneNameLikeAndCapitalGreaterThan() {
+		String name = BASE_AMUSEMENT_PARK.getName() + "1";
 		int capital = 14000;
 
-		Specification<AmusementPark> spec = like("name", BASE_AMUSEMENT_PARK.getName() + "1%");
-		spec = spec.and(greaterThan("capital", capital));
+		AmusementPark amusementPark = amusementParkRepository.findOne((Root<AmusementPark> root, CriteriaQuery<?> query,
+				CriteriaBuilder cb) -> cb.and(cb.like(root.get(AmusementPark_.name), name + "%"),
+						cb.gt(root.get(AmusementPark_.capital), capital)))
+				.get();
 
-		AmusementPark amusementPark = amusementParkRepository.findOne(spec).get();
-		assertTrue(amusementPark.getName().startsWith(BASE_AMUSEMENT_PARK.getName() + "1"));
+		assertTrue(amusementPark.getName().startsWith(name));
 		assertTrue(amusementPark.getCapital() > capital);
 	}
 
@@ -133,15 +139,14 @@ public class JPASpecificationTests {
 	public void findOneNameLikeAndCapitalGreaterThanAndAddressCityLike() {
 		int capital = 14000;
 
-		Specification<AmusementPark> spec = (Root<AmusementPark> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
-			root.fetch("address");
-			return null;
-		};
+		AmusementPark amusementPark = amusementParkRepository
+				.findOne((Root<AmusementPark> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+					root.fetch(AmusementPark_.address);
+					return cb.and(cb.like(root.get(AmusementPark_.name), BASE_AMUSEMENT_PARK.getName() + "1%"),
+							cb.gt(root.get(AmusementPark_.capital), capital),
+							cb.like(root.get(AmusementPark_.address).get(Address_.city), "Buda%"));
+				}).get();
 
-		spec = spec.and(like("name", BASE_AMUSEMENT_PARK.getName() + "1%")).and(greaterThan("capital", capital))
-				.and(like("address.city", "Buda%"));
-
-		AmusementPark amusementPark = amusementParkRepository.findOne(spec).get();
 		assertTrue(amusementPark.getName().startsWith(BASE_AMUSEMENT_PARK.getName() + "1"));
 		assertTrue(amusementPark.getCapital() > capital);
 		assertTrue(amusementPark.getAddress().getCity().startsWith("Buda"));
@@ -154,19 +159,20 @@ public class JPASpecificationTests {
 			Subquery<Machine> subQuery = query.subquery(Machine.class);
 			Root<Machine> subRoot = subQuery.from(Machine.class);
 			subQuery.select(subRoot);
-			subQuery.where(cb.equal(root.get("id"), subRoot.get("amusementPark").get("id")),
-					cb.lt(subRoot.get("minimumRequiredAge"), 18));
-			query.orderBy(cb.asc(root.get("id")));
+			subQuery.where(
+					cb.equal(root.get(AmusementPark_.id), subRoot.get(Machine_.amusementPark).get(AmusementPark_.id)),
+					cb.lt(subRoot.get(Machine_.minimumRequiredAge), 18));
+			query.orderBy(cb.asc(root.get(AmusementPark_.id)));
 			return cb.exists(subQuery);
 		};
 
 		List<AmusementPark> resultWithSubQuery = amusementParkRepository.findAll(spec);
 
 		spec = (Root<AmusementPark> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
-			Join<AmusementPark, Machine> joinMachines = root.join("machines");
-			joinMachines.on(cb.lt(joinMachines.get("minimumRequiredAge"), 18));
+			Join<AmusementPark, Machine> joinMachines = root.join(AmusementPark_.machines);
+			joinMachines.on(cb.lt(joinMachines.get(Machine_.minimumRequiredAge), 18));
 			query.distinct(true);
-			query.orderBy(cb.asc(root.get("id")));
+			query.orderBy(cb.asc(root.get(AmusementPark_.id)));
 			return null;
 		};
 
@@ -179,29 +185,4 @@ public class JPASpecificationTests {
 		assertEquals(amusementParksWithMachineForChildren, resultWithJoin);
 
 	}
-
-	private <T> Specification<T> like(String fieldName, String param) {
-		return (Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> cb.like(createPath(root, fieldName),
-				param);
-	}
-
-	private <T> Specification<T> greaterThan(String fieldName, int param) {
-		return (Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> cb.greaterThan(createPath(root, fieldName),
-				param);
-	}
-
-	private <T, Y> Path<Y> createPath(Root<T> root, String fieldName) {
-		Path<Y> path = null;
-		if (fieldName.indexOf('.') == -1) {
-			path = root.get(fieldName);
-		} else {
-			String[] parts = fieldName.split("\\.");
-			path = root.get(parts[0]);
-			for (int i = 1; i < parts.length; i++) {
-				path = path.get(parts[i]);
-			}
-		}
-		return path;
-	}
-
 }
