91cfea7c37055e73a4e981c52f9b662b431aef32
Kovács Jenifer <fenicser85@gmail.com>
2019-07-07 16:50:13 +0200

    Machine get on and off, carousel image.

--- amusement-park-monolithic/src/main/resources/public/js/machine.js
+++ amusement-park-monolithic/src/main/resources/public/js/machine.js
@@ -18,8 +18,8 @@ function getMachines(href) {
 
 function machineFillTableWithData(data) {
 	var tableBody = []
-	
-	if (data._embedded !== undefined){
+
+	if (data._embedded !== undefined) {
 		$.each(data._embedded.machineResourceList, function(i, e) {
 			tableBody.push(convertMachineToTableRow(e))
 		})
@@ -38,7 +38,40 @@ function convertMachineToTableRow(machine) {
 	tr.push("<td>" + machine.minimumRequiredAge + "</td>")
 	tr.push("<td>" + machine.ticketPrice + "</td>")
 	tr.push("<td>" + machine.type.toLowerCase() + "</td>")
-	tr.push("<td>"+"</td>")
+	tr
+			.push("<td> <input type=\"button\" value=\"Get on\" "
+					+ "onclick=\"getOnMachine('"
+					+ machine._links.getOnMachine.href + "','"
+					+ machine.fantasyName + "','" + machine.type + "')\"></td>")
 	tr.push("</tr>")
 	return tr.join("")
+}
+function getOnMachine(href, name, type) {
+	$.ajax({
+		url : href,
+		method : "PUT",
+		success : function(data) {
+			$("#machineModal").modal("show")
+			$("#machineModalTitle").html(name)
+			switch (type) {
+			case "CAROUSEL":
+				$("#machineModalImage").attr("src", "/img/carousel.jpg")
+				break
+			}
+			$("#spendingMoney").html(data.spendingMoney)
+			$("#machineModalExit").attr("onclick","getOffMachine('" + data._links.getOffMachine.href + "')")
+			$("#machineModalGetOff").attr("onclick","getOffMachine('" + data._links.getOffMachine.href + "')")
+		}
+	})
+}
+function getOffMachine(href){
+	$.ajax({
+		url : href,
+		method : "PUT",
+		success : function(data) {
+			$("#machineModal").modal("hide")
+			$("#machineModalTitle").html("")
+			$("#machineModalImage").attr("src", "")
+		}
+	})
 }
