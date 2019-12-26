af1f8a2117afe25ed461886074721f433998c48b
Kovács Jenifer <fenicser85@gmail.com>
2019-12-26 13:29:16 +0100

    Close Amusement Park create modal on succesful save.

--- amusement-park-monolithic/src/main/resources/public/js/amusement-park.js
+++ amusement-park-monolithic/src/main/resources/public/js/amusement-park.js
@@ -16,66 +16,44 @@ function toggleSearchDiv(){
 }
 
 function save() {
-	$("#save").attr("disabled", true)
+	$("#createParkSave").attr("disabled", true)
 	$.ajax({
 		url : links.amusementPark,
 		method : "POST",
 		contentType : "application/json",
 		data : JSON.stringify(parkCollectData()),
-		success : function(response) {
-			var result = $("#result")
-			result.html("success")
-			result.addClass("text-success")
-			result.show()
+		success : function() {
+			$("#createButtonModal").modal("hide")
 			getAmusementParks()
 		},
 		error : function(response) {
-			var result = $("#result")
+			var result = $("#createHibaUzenet")
 			result.html("error: " + response.responseText)
 			result.addClass("text-danger")
-			result.show()
 		},
-		complete : function(response) {
-			$("#save").attr("disabled", false)
-			setTimeout(function() {
-				var result = $("#result")
-				result.hide()
-				result.removeClass()
-			}, 8000)
+		complete : function() {
+			$("#createParkSave").attr("disabled", false)
 		}
 	})
 }
 
 function parkCollectData() {
-	var address = {}
-	address.country = $("#country").val()
-	address.zipCode = $("#zipCode").val()
-	address.city = $("#city").val()
-	address.street = $("#street").val()
-	address.houseNumber = $("#houseNumber").val()
-
 	var amusementPark = {}
-	amusementPark.name = $("#parkName").val()
-	amusementPark.capital = $("#capital").val()
-	amusementPark.totalArea = $("#totalArea").val()
-	amusementPark.entranceFee = $("#entranceFee").val()
+	amusementPark.name = $("#createInputName").val()
+	amusementPark.capital = $("#createInputCapital").val()
+	amusementPark.totalArea = $("#createInputTotalArea").val()
+	amusementPark.entranceFee = $("#createInputEntranceFree").val()
+	
+	var address = {}
+	address.zipCode = 1000
+	address.country="Budapest"
+	address.street="Peterdy"
+	address.city="Valami"
+	address.houseNumber="23"
 	amusementPark.address = address
-
 	return amusementPark
 }
 
-function parkFillWithSampleData() {
-	$("#parkName").val("Beni parkja")
-	$("#capital").val("10000")
-	$("#totalArea").val("1000")
-	$("#entranceFee").val("50")
-
-	$("#country").val("Magyarország")
-	$("#zipCode").val("1148")
-	$("#city").val("Budapest")
-	$("#street").val("Fogarasi út")
-	$("#houseNumber").val("80/C")
-}
 
 function getAmusementParks() {
 	$.ajax({
@@ -105,10 +83,6 @@ function convertAmusementParkToTableRow(amusementPark) {
 	tr.push("<td>" + amusementPark.capital + "</td>")
 	tr.push("<td>" + amusementPark.totalArea + "</td>")
 	tr.push("<td>" + amusementPark.entranceFee + "</td>")
-	var address = amusementPark.address
-	tr.push("<td>" + address.country + " " + address.zipCode)
-	tr.push(" " + address.city + " " + address.street)
-	tr.push(" " + address.houseNumber + "</td>")
 	tr.push("<td> <input type=\"button\" value=\"Enter\" onclick=\"enterPark('"
 			+ amusementPark._links.visitorEnterPark.href + "','" + amusementPark._links.machine.href + "')\"></td>")
 	if (isAdmin) {
@@ -118,6 +92,7 @@ function convertAmusementParkToTableRow(amusementPark) {
 	tr.push("</tr>")
 	return tr.join("")
 }
+
 function enterPark(href,machineHref) {
 	$.ajax({
 		url : href,
