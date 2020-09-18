c863b244a4813d78c52beffc2696b7563eb00f8b
Bence Németh <nembence1994@gmail.com>
2020-09-18 17:43:49 +0200

    Remove leftover button.

--- micro/gateway/src/main/java/hu/beni/gateway/controller/UserController.java
+++ micro/gateway/src/main/java/hu/beni/gateway/controller/UserController.java
@@ -1,11 +1,7 @@
 package hu.beni.gateway.controller;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.security.Principal;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +11,6 @@ public class UserController {
 	@GetMapping("/user")
 	public Principal getUser(Principal principal) {
 		return principal;
-	}
-
-	/*@GetMapping("/")
-	public String main() throws IOException {
-		return StreamUtils.copyToString(new ClassPathResource("public/index.html").getInputStream(),
-				StandardCharsets.UTF_8);
-	}*/
+	}	
 
 }
