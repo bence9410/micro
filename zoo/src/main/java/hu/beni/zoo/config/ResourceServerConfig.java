05b538516b0ba5664ae3aff6f51b90821ac8c6cc
Bence Németh <nembence1994@gmail.com>
2020-09-20 19:13:58 +0200

    Move out from second micro folder.

---
+++ zoo/src/main/java/hu/beni/zoo/config/ResourceServerConfig.java
@@ -0,0 +1,32 @@
+package hu.beni.zoo.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public RemoteTokenServices remoteTokenServices() {
+		RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
+		remoteTokenServices.setRestTemplate(restTemplate());
+		remoteTokenServices.setCheckTokenEndpointUrl("http://oauth2/uaa/oauth/check_token");
+		remoteTokenServices.setClientId("beni");
+		remoteTokenServices.setClientSecret("benisecret");
+		return remoteTokenServices;
+	}
+
+}
